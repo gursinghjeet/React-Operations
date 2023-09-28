ad6980ddfbc98d48681fa1b3360c3184247e1861
Gurjeet Singh <hit19ece.gurjeetsingh@gmail.com>
2023-09-29 00:26:22 +0530

    React Day-2

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
-
+import Chai from "./chai";
 
 function App() {
   return (
+    <>
     <h1>Hello World</h1>
+    <Chai />
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,14 @@
+import Chai from "./chai"
 
 function App() {
 
   return (
+    <div>
     <h1>Hello Gurjeet You are Welcome here to learn ReactJS</h1>
-  )
+    <Chai/>
+    <p>virat kohli is my idol</p>
+    </div>
+    )
 }
 
 export default App
