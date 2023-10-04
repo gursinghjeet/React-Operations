c3964ccb613120a86e85f0e2c12c4c176b07450e
Gurjeet Singh <hit19ece.gurjeetsingh@gmail.com>
2023-10-05 01:01:44 +0530

    React Day-4

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,25 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 function NewApp(){
+  const part = ' you are welcome'
   return(
-    <h1>Hello New Apps</h1>
+    <h1>Hello New Apps{part}</h1>
   )
 }
 
 
-const reactElement = {
-  type: 'a',
-  props: {
-      href: 'https://google.com',
-      target: '_blank'
-  },
-  children: 'Click me to visit google'
-}
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
 
 const AnotherApp = (        
 
     <a href="https://google.com">visit google from here</a>
-  
+
+)
+
+const substring = ' from here'
+
+const reactElement = React.createElement(
+  'a',
+  {href : "https://google.com",targetm : '_blank'},
+  'Click me to visit google',
+  substring
 )
 
 
@@ -30,7 +40,8 @@ render(
   
     // <App />
     // <NewApp/>
-    AnotherApp
+    // AnotherApp
+    reactElement
     
 
 )
