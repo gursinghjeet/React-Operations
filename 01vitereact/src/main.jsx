4a047fab1a6e043d7b82ec0f05d5f314351f194f
Gurjeet Singh <hit19ece.gurjeetsingh@gmail.com>
2023-10-04 23:31:58 +0530

    React Day-3

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function NewApp(){
+  return(
+    <h1>Hello New Apps</h1>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const reactElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
+
+const AnotherApp = (        
+
+    <a href="https://google.com">visit google from here</a>
+  
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  
+    // <App />
+    // <NewApp/>
+    AnotherApp
+    
+
 )
--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,10 +1,19 @@
 function customRender(reactElement, container){
 
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
 
+    // container.appendChild(domElement)
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML  = reactElement.children
+
+    for (const prop in reactElement.props) { //set attribute using for in loop
+        if(prop === 'children')continue;
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
     container.appendChild(domElement)
 }
 const reactElement = {
