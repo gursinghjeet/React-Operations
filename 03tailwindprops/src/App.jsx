b75cc04fefc1b0c0b55309704bd3be18e93695a0
Gurjeet Singh <hit19ece.gurjeetsingh@gmail.com>
2023-10-07 20:13:15 +0530

    Raect Day-6

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-800 text-black p-4 rounded-xl mb-4'>TailWind Test</h1>
+
+      <Card/>
+      <Card/>
+
+
+<h2
+className='bg-yellow-600 p-4 font-bold text-xl rounded-2xl text-blue-950 mt-4'
+>These is a footer</h2>
+    </>
+  )
+}
+
+export default App
